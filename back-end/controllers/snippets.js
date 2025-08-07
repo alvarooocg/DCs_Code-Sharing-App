a2368bc36dc32fa418aaf57a725c5d8bd917394e
alvaro <103962133+alvarooocg@users.noreply.github.com>
2025-08-07 11:34:41 +0200

    .

--- back-end/controllers/snippets.js
+++ back-end/controllers/snippets.js
@@ -1,7 +1,5 @@
-const middleware = require('../utils/middleware')
 const snippetsRouter = require('express').Router()
 const Snippet = require('../models/snippet')
-const next = require('next')
 
 snippetsRouter.get('/', async (request, response, next) => {
     try {
