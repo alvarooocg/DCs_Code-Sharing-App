ba8d149401a33d63e1b75784df94d432bcf08a98
alvaro <103962133+alvarooocg@users.noreply.github.com>
2025-08-07 12:06:48 +0200

    Next alternative use

--- back-end/controllers/snippets.js
+++ back-end/controllers/snippets.js
@@ -1,25 +1,29 @@
 const snippetsRouter = require('express').Router()
 const Snippet = require('../models/snippet')
 
-snippetsRouter.get('/', async (request, response, next) => {
+snippetsRouter.get('/', async (request, response) => {
     try {
         const snippets = await Snippet.find({})
         response.json(snippets)
     } catch(error) {
-        next(error)
+        console.error('Error =>', error)
+        res.status(500).json({ error: 'Something went wrong' })
     }
 })
 
-snippetsRouter.get('/:id', (request, response, next) => {
+snippetsRouter.get('/:id', (request, response) => {
     Snippet.findById(request.params.id)
         .then(snippet => {
             if (snippet) response.json(snippet)
             else response.status(404).send({ error: 'Not found' })
         })
-        .catch(err => next(err))
+        .catch(err => {
+            console.error('Error =>', err)
+            res.status(500).json({ error: 'Something went wrong' })
+        })
 })
 
-snippetsRouter.post('/', async (request, response, next) => {
+snippetsRouter.post('/', async (request, response) => {
     try {
         const body = request.body
 
@@ -35,7 +39,8 @@ snippetsRouter.post('/', async (request, response, next) => {
         const newSnippet = await snippet.save()
         response.status(201).json(newSnippet)
     } catch (error) {
-        next(error)
+        console.error('Error =>', error)
+        res.status(500).json({ error: 'Something went wrong' })
     }
 })
 
